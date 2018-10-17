0ce8dc965763f6d463763c1e3fb0142f13121287
salva116ario <salvatore.sancesario.lp2@gmail.com>
2018-10-17 15:13:52 +0200

    tirageAuSort added

--- Puissance4.java
+++ Puissance4.java
@@ -5,22 +5,33 @@ public class Puissance4 {
   static Scanner saisie;
   static int nbCol = 7, nbLig =6;
   static int[][] grille = new int[nbCol][nbLig];
-  static String[] joueur = new String[2];
+  static String[] joueur = new String[3];
+  static int joueurEnCours = 1;
 
 public static void main(String[] args) {
   saisie = new Scanner(System.in);
   initJoueurs();
+  tirageAuSort();
 
 
 }
 
   public static void initJoueurs() {
-    for (int i=0; i<2; i++) {
-      System.out.println("Joueur " + (i+1) + " entrez votre prénom : ");
+    for (int i=1; i<3; i++) {
+      System.out.println("Joueur " + (i) + " entrez votre prénom : ");
       joueur[i] = saisie.nextLine();
     }
-    System.out.println(joueur[0] + " / " + joueur[1]);
+    System.out.println(joueur[1] + " / " + joueur[2]);
   }
 
+  public static void tirageAuSort() {
+    int hasard = (int)(Math.random() * 10);
+    System.out.println(hasard);
+    if (hasard%2==0) {
+      joueurEnCours = 1;}
+    else { joueurEnCours = 2;}
+    System.out.println("C'est " + joueur[joueurEnCours] + " qui commence");
+  }
 
+  
 }
