43f953ac0fbd7ced0598b64a95954daa1ad95664
salva116ario <salvatore.sancesario.lp2@gmail.com>
2018-10-17 15:38:14 +0200

    affichageGrille added

--- Puissance4.java
+++ Puissance4.java
@@ -7,11 +7,13 @@ public class Puissance4 {
   static int[][] grille = new int[nbCol][nbLig];
   static String[] joueur = new String[3];
   static int joueurEnCours = 1;
+  static int compteur = 0;
 
 public static void main(String[] args) {
   saisie = new Scanner(System.in);
   initJoueurs();
   tirageAuSort();
+  affichageGrille();
 
 
 }
@@ -31,7 +33,33 @@ public static void main(String[] args) {
       joueurEnCours = 1;}
     else { joueurEnCours = 2;}
     System.out.println("C'est " + joueur[joueurEnCours] + " qui commence");
+    joueurEnCours =( joueurEnCours == 1) ? 2 : 1;
   }
 
-  
+  public static void affichageGrille() {
+
+      compteur++;
+      System.out.println("\nTOUR N°"+compteur);
+      joueurEnCours =( joueurEnCours == 1) ? 2 : 1;
+      System.out.println("C'est à " + joueur[joueurEnCours] + " de jouer.");
+      char jeton = ' ';
+
+      for (int i=0; i<nbLig; i++) {
+        for (int j=0; j<nbCol; j++) {
+          if (grille[j][i] == 0) {
+            jeton = ' ';
+            }
+          if (grille[j][i] == 1) {
+            jeton = '1';
+          }
+          if (grille[j][i] == 2) {
+            jeton = '2';
+          }
+          System.out.print("| "+jeton+" ");
+        }
+        System.out.println("|");
+      }
+      System.out.println("| 0 | 1 | 2 | 3 | 4 | 5 | 6 |");
+  }
+
 }
